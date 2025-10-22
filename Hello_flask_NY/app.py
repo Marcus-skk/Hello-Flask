fd4386d3c93675612c56b154b56a1707c863ac8c
Marcus-skk <mar30048@skole.telemarkfylke.no>
2025-10-22 09:08:27 +0200

    Del 2

--- Hello_flask_NY/app.py
+++ Hello_flask_NY/app.py
@@ -1,10 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+@app.route("/kontakt", methods=["GET", "POST"])
+def kontakt():
+    if request.method == "POST":
+        email = request.form.get("email")
+        tittel = request.form.get("tittel")
+        melding = request.form.get("melding")
+        tema = request.form.get("tema")
+
+        # Midlertidig respons
+        return f"Takk for din henvendelse, {email}! Vi vil svare på {tema} snart."
+
+    return render_template("kontakt.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
